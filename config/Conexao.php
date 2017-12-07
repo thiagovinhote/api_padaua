4a97aa1449dec79e06c8019db076aae4c6c884b3
Thiago Vinhote <thiagovf26@outlook.com>
2017-12-07 18:03:22 -0400

    conexão
    Signed-off-by: Thiago Vinhote <thiagovf26@outlook.com>

--- config/Conexao.php
+++ config/Conexao.php
@@ -1,24 +1,26 @@
 <?php
-
 class Conexao {
-
   private static $shared;
   private $db;
-
   private function Conexao(){
-    $this->db = new mysqli("localhost", "root", "", "dbpadaua");
+    $url = getenv('JAWSDB_MARIA_URL');
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+
+    $this->db = new mysqli($hostname, $username, $password, $database);
     $this->db->set_charset('utf8');
   }
-
   public static function getShared() {
     if (self::$shared == null) {
       self::$shared = new Conexao();
     }
     return self::$shared;
   }
-
   public function getDB() {
     return $this->db;
   }
-
 }
